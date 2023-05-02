ea58900c0160ee618a068a9da5c89a0f7699d557
jase863 <walker.jason8630@gmail.com>
2023-05-01 18:59:11 -0600

    Finished program and class

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction f1 = new Fraction();
+
+        Console.Write("Top number please: ");
+
+        int givenTop = int.Parse(Console.ReadLine());
+
+        Console.Write("Bottom number please: ");
+
+        int givenBottom = int.Parse(Console.ReadLine());
+
+        f1.SetTop(givenTop);
+        f1.SetBottom(givenBottom);
+
+        Console.WriteLine(f1.GetFractionString());
+
+        Console.WriteLine(f1.GetDecimalValue());
     }
 }
