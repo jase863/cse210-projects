70cb67fbf184c64250427fdf923608ceb783d2df
jase863 <walker.jason8630@gmail.com>
2023-05-16 20:36:17 -0600

    completed shapes learning activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>{};
+
+        Square square = new Square("blue", 5);
+        shapes.Add(square);
+
+        Rectangle rectangle = new Rectangle("green", 7, 4);
+        shapes.Add(rectangle);
+
+        Circle circle = new Circle("pink", 2);
+        shapes.Add(circle);
+
+        
+
+        foreach(Shape shape in shapes)
+        {   
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+            
+            Console.WriteLine($"The {color} shape has an area of: {area} units squared");
+        }
+
     }
 }
